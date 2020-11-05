2b599b841b77f3f9a27775ea548d05f8a3266089
janob sulaymon <sulaymon1w@gmail.com>
2020-11-05 18:50:43 +0500

    asdfasdfasdf

--- src/main/java/uz/pdp/botsale/controller/MarketController.java
+++ src/main/java/uz/pdp/botsale/controller/MarketController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uz.pdp.botsale.payload.ApiResponse;
 import uz.pdp.botsale.payload.ReqMarket;
 import uz.pdp.botsale.service.MarketService;
@@ -24,4 +21,5 @@ public class MarketController {
         ApiResponse response = marketService.saveOrEdit(reqMarket);
         return ResponseEntity.status(response.isSuccess() ? response.getMessage().equals("Saved") ? HttpStatus.CREATED : HttpStatus.ACCEPTED : HttpStatus.CONFLICT).body(response);
     }
+//    @GetMapping
 }
